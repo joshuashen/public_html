574edb82396a8d91d094193bbca4c59b66998a1e
Serious Adverse Events Consortium % Floratos <saec@gaia.c2b2.columbia.edu>
2009-05-20 15:03:16 -0400

    qqplot R function

--- qqplot-function.R
+++ qqplot-function.R
@@ -53,7 +53,7 @@ pQQplot <- function(pvalfile=NULL, data = NULL)
            # main = mainqq,
          xlab = "-Log10 Expected P-values",
          ylab = "-Log10 Observed P-values", col='red',
-         pch = 19)
+         pch = 20)
 
   explimit = data$expected[data$expected > 0.00000005]
   
